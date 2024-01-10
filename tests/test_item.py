5a4a75c6481513cbb9d5285f21132ccbc995cc69
Krimsi13 <aleksejkrimsi@gmail.com>
2024-01-10 22:23:18 +0300

    hw1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+item1 = Item("Banana", 8.95, 4)
+
+
+def test_case1():
+    assert item1.calculate_total_price() == 35.8
+
+
+def test_case2():
+    item1.apply_discount()
+    assert 8.95 * Item.pay_rate == item1.price
